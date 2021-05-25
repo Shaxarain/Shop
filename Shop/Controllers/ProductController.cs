b92ce1f015239ed5dd0d8f8fb4fe0c846c2269ea
Shaxarain <RetroLOL2>
2021-05-25 22:01:06 +0300

    Paaaages

--- Shop/Controllers/ProductController.cs
+++ Shop/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using Funcs;
+using Shop.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,17 +11,28 @@ namespace Shop.Controllers
     public class ProductController : Controller
     {
         UOWCatalog uow;
+        public int PageSize = 8;
         public ProductController()
         {
             uow = new UOWCatalog();
         }
-        public ActionResult Index()
+        public ActionResult Index(int page = 1)
         {
-            var catalog = uow.Product.GetAll().Where(p => p.ProductInventory.Sum(q => q.Quantity) > 0 && p.StandardCost != 0);
-            ViewBag.Catalog = catalog;
-            /*IEnumerable<Product> prods = prodRepo.GetWithInclude(pp => pp.ProductProductPhoto.ProductPhoto));
-            ViewBag.Productions = prods;*/
-            return View();
+            ProductsViewModel model = new ProductsViewModel
+            {
+                Products = uow.Product.GetAll()
+                .Where(p => p.ProductInventory.Sum(q => q.Quantity) > 0 && p.StandardCost != 0)
+                .OrderBy(p => p.ProductID)
+                .Skip((page - 1) * PageSize)
+                .Take(PageSize),
+                PageInfo = new PageInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = uow.Product.GetAll().Where(p => p.ProductInventory.Sum(q => q.Quantity) > 0 && p.StandardCost != 0).Count()
+                }
+            };
+            return View(model);
         }
     }
 }
---
+++ Shop/Models/ProductsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using DBs.Production;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Shop.Models
+{
+    public class ProductsViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public PageInfo PageInfo { get; set; }
+    }
+}
